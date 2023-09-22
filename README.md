e421422425f6d44e67f9cb4249316c1e25e05018
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2023-09-22 13:50:59 -0400

    Starting over.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # WikiScraper
 
-Scrapy spider to scrape all the wikipedia pages in https://new.wikipedia.org/wiki in this case and output the results into a csv file.
+Scrapy spider to scrape all the wikipedia article pages in https://new.wikipedia.org/wiki and output the results into a csv file.
 
-The spider will pull the data from the first description in the page. Any page without a description will be ignored.
+The spider will start from the first page of the "all pages" directory.
+
+The spider will pull the data from the first description in the page. Any page without a description will be ignored. Any page where the article does not meet the length requirement will also be ignored.
--- wikicrawler/wikiscraper/spiders/portal.py
+++ wikicrawler/wikiscraper/spiders/portal.py
@@ -1,35 +1,11 @@
-# spider to pull links from portal page (https://new.wikipedia.org/wiki/%E0%A4%AE%E0%A5%82_%E0%A4%AA%E0%A5%8C) and saves them to a file.
+# spider to crawl all links in article directory starting from the portal page.
 
 import scrapy
-from scrapy import Request
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from ..items import PortalItem
 
 class PortalSpider(CrawlSpider):
-    name = 'portal'
-
-    # restricts the crawl to pages within the new.wikipedia.org domain
-    allowed_domains = ['new.wikipedia.org']
-    start_urls = ['https://new.wikipedia.org/wiki/%E0%A4%AE%E0%A5%82_%E0%A4%AA%E0%A5%8C']
-
-    # uses a LinkExtractor to find links to other pages within the same domain
-    # and in the 1st, 3rd, and 4th table in <div class="mw-parser-output">
-    rules = (
-        Rule(LinkExtractor(allow=r'https://new.wikipedia.org/wiki/', 
-                           restrict_xpaths=['//div[3]//div[3]//table[1]', '//div[3]//div[3]//table[3]', '//div[3]//div[3]//table[4]']), 
-                           callback='parse_info', follow=False),    
-    )
-
-    # parses the response, ignores page if it has already been visited,
-    # and saves url to json file
-    def parse_info(self, response):
-        item = PortalItem()
-        item['url'] = response.url
-        yield item
-
-# Run this spider with the following command:
-# scrapy crawl portal -o links.csv:csv
-
+    name = "portal"
+    allowed_domains = ["https://new.wikipedia.org/"]
+    start_urls = ['https://new.wikipedia.org/w/index.php?title=विशेष:AllPages&from="अपूर्व+चकोतरर्कळ्+(तमिल+संकिपा)']
     
-
