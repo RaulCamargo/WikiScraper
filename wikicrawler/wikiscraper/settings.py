3582eb215ae1b0a7241cdb0c88329c572a9386e2
Raul Camargo <kmargo945@gmail.com>
2023-12-27 11:10:23 -0500

    + filter to exclude url with latin script in path

--- wikicrawler/wikiscraper/settings.py
+++ wikicrawler/wikiscraper/settings.py
@@ -20,12 +20,12 @@ NEWSPIDER_MODULE = "wikiscraper.spiders"
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -50,9 +50,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "wikiscraper.middlewares.WikiscraperDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+  'wikiscraper.project_middleware.latin_filter.LatinFilterMiddleware': 543,
+}
+
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -62,9 +63,12 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "wikiscraper.pipelines.WikiscraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+  "wikiscraper.pipelines.WikiscraperPipeline": 300,
+    
+  "wikiscraper.pipelines.DatePipeline": 400,
+}
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -72,10 +76,10 @@ AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 10
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
@@ -100,3 +104,6 @@ FEEDS = {
         'overwrite': True,
     }
 }
+
+# Set the DUPEFILTER_CLASS setting to ignore duplicate requests
+DUPEFILTER_CLASS = 'scrapy.dupefilters.RFPDupeFilter'
