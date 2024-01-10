e9b8069da5cd0bd0dd35a6cdc758a38082c58ac1
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2024-01-10 13:44:10 -0500

    Fixed issues with date

--- wikicrawler/wikiscraper/spiders/portal.py
+++ wikicrawler/wikiscraper/spiders/portal.py
@@ -10,33 +10,45 @@ class PortalSpider(CrawlSpider):
     start_urls = ['https://new.wikipedia.org/wiki/%E0%A4%AE%E0%A5%82_%E0%A4%AA%E0%A5%8C']
 
     # rules to only follow links in /wiki/ directory
-    # and ignore URLs with latin script
+    
     rules = (
-        Rule(LinkExtractor(allow=('/wiki/[\u0900-\u097F]',)), process_links='filter_links', callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=('/wiki/',)), process_links='filter_links', callback='parse_item', follow=True),
     )
     
-    # parse the page and save the date last modified,
-    # save the url,
-    # save the title,
-    # save text from the first <p> tag in <div class="mw-parser-output"> if it contains more than 3 "|" characters
-    def parse_item(self, response):
-        if response.css('div.mw-parser-output p::text').getall():
-            if response.css('div.mw-parser-output p::text').getall()[0].count('|') > 3:
-                yield {
-                    'date': response.css('li#footer-info-lastmod::text').get(),
-                    'url': response.url,
-                    'title': response.css('h1#firstHeading::text').get(),
-                    'text': response.css('div.mw-parser-output p::text').getall()[0]
-                }
-    
-    # filter out links with latin script
     def filter_links(self, links):
-        deny_pattern = '/wiki/[a-zA-Z]'
-        return [link for link in links if deny_pattern not in link.url]
+        # This method allows only links within the specified section.
+        # Adjust the selectors based on your HTML structure.
+
+        # allowed_links = []
+
+        # allowed_section_selector = 'div#mw-content-text.mw-body-content'  # CSS selector for the allowed section
+
+        # for link in links:
+        #     if link.url.startswith(tuple(self.start_urls)) or link.url.endswith('robots.txt') or (
+        #             response and response.css(allowed_section_selector)):
+        #         allowed_links.append(link)
+
+        return links
+
+
+    def parse_item(self, response):
+        allowed_section_selector = 'div#mw-content-text.mw-body-content'  # CSS selector for the allowed section
+
+        if (
+            response.url.startswith(tuple(self.start_urls)) or
+            response.url.endswith('robots.txt') or
+            response.css(allowed_section_selector)
+        ):
+            yield {
+                'date': response.css('li#footer-info-lastmod::text').get(),
+                'url': response.url,
+                'title': response.css('span.mw-page-title-main::text').get(),
+                'text': response.css('div.mw-parser-output p::text').getall()[0]
+            }
     
-    # print response.title to console
-    def parse(self, response):
-        print(response.title)
+    # # print response.title to console
+    # def parse(self, response):
+    #     print(response.title)
 
     
     
